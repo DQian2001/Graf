6c542fcde061a0cf5293ffb8a430e8b3f2f557a2
Alice B <60297067+semicolon-madness@users.noreply.github.com>
2021-08-09 17:28:27 -0400

    more formatting
    for some reason it runs on this ???

--- Vue/graf/src/components/Toolbar.vue
+++ Vue/graf/src/components/Toolbar.vue
@@ -100,10 +100,10 @@ export default{
         },
         "Algorithm" : {
           "bfs" : "Breadth First Search (BFS)",
-	  "dfs" : "Depth First Search (DFS)",
+	 "dfs" : "Depth First Search (DFS)",
 	  "dijkstra" : "Dijkstra",
           "kosaraju" : "Kosaraju's"
-	  "bellmanford" : "Bellman Ford",
+	 "bellmanford" : "Bellman Ford",
 	  "maxFlow" : "Maximum Flow"
         }
       },
